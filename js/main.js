315089f24b2616c37b54faa920363238ea3b23d7
AishiHuangUIUC <90989610+AishiHuangUIUC@users.noreply.github.com>
2024-07-17 10:55:15 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,23 @@
 // main.js
 
 let currentScene = 1;
+let olympicData = null;
 
 function updateScene() {
   if (currentScene === 1) {
-    renderScene1();
+    renderScene1(olympicData);
   } else if (currentScene === 2) {
-    renderScene2();
+    renderScene2(olympicData);
   } else if (currentScene === 3) {
-    renderScene3();
+    renderScene3(olympicData);
   }
 }
+
+// Load data and initialize visualization
+d3.csv("data/athlete_events.csv").then(data => {
+  olympicData = data;
+  updateScene();
+});
 
 document.addEventListener("keydown", (event) => {
   if (event.key === "ArrowRight") {
@@ -18,5 +25,3 @@ document.addEventListener("keydown", (event) => {
     updateScene();
   }
 });
-
-updateScene();
