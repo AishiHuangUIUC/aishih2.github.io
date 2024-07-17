1783541579d51e3c24642ffc169a2f327f9be2bd
AishiHuangUIUC <90989610+AishiHuangUIUC@users.noreply.github.com>
2024-07-17 14:42:59 -0500

    Update scene1.js

--- js/scene1.js
+++ js/scene1.js
@@ -1,135 +1,84 @@
 // Load the CSV data
 d3.csv("data/athlete_events.csv").then(function(data) {
-  // Extract the year from the "Year" field and convert it to a number
-  data.forEach(function(d) {
-    d.Year = +d.Year;
+  // Parse data
+  data.forEach(d => {
+    d.Age = +d.Age;
+    d.Height = +d.Height;
+    d.Weight = +d.Weight;
   });
 
-  const years = Array.from(new Set(data.map(d => d.Year))).sort();
-
-  // Populate the year dropdown
-  const select = d3.select("#yearFilter");
-  years.forEach(year => {
-    select.append("option").text(year).attr("value", year);
-  });
-
-  // Define the color scale for the years
-  const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
-
-  // Define the dimensions and margins of the plot
-  const margin = { top: 50, right: 50, bottom: 100, left: 80 };
+  const margin = { top: 50, right: 30, bottom: 100, left: 60 };
   const width = 800 - margin.left - margin.right;
   const height = 600 - margin.top - margin.bottom;
 
-  // Create the SVG container for the plot
-  const svg = d3
-    .select("#plot-container")
+  const svg = d3.select("#plot-container")
     .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  const tooltip = d3
-    .select("body")
-    .append("div")
-    .attr("class", "tooltip");
+  // X axis
+  const x = d3.scaleLinear()
+    .domain(d3.extent(data, d => d.Age))
+    .nice()
+    .range([0, width]);
 
-  function updatePlot(selectedYear) {
-    let filteredData = data;
-    if (selectedYear !== "all") {
-      filteredData = data.filter(d => d.Year == selectedYear);
-    }
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x))
+    .append("text")
+    .attr("class", "axis-title")
+    .attr("x", width)
+    .attr("y", -10)
+    .style("text-anchor", "end")
+    .text("Age");
 
-    const participation = d3.rollup(filteredData, v => v.length, d => d.Year);
-    const years = Array.from(participation.keys());
-    const counts = Array.from(participation.values());
+  // Y axis
+  const y = d3.scaleLinear()
+    .domain(d3.extent(data, d => d.Height))
+    .nice()
+    .range([height, 0]);
 
-    const x = d3.scaleBand()
-      .domain(years)
-      .range([0, width])
-      .padding(0.1);
-
-    const y = d3.scaleLinear()
-      .domain([0, d3.max(counts)])
-      .nice()
-      .range([height, 0]);
-
-    svg.selectAll("g").remove();
-    svg.selectAll(".bar").remove();
-
-    svg.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
-
-    svg.append("g")
-      .call(d3.axisLeft(y));
-
-    svg.selectAll(".bar")
-      .data(years)
-      .enter()
-      .append("rect")
-      .attr("class", "bar")
-      .attr("x", d => x(d))
-      .attr("y", d => y(participation.get(d)))
-      .attr("width", x.bandwidth())
-      .attr("height", d => height - y(participation.get(d)))
-      .attr("fill", "steelblue");
-  }
-
-  // Initially load with all data
-  updatePlot("all");
-
-  // Add event listener to dropdown
-  d3.select("#yearFilter").on("change", function() {
-    const selectedYear = d3.select(this).property("value");
-    updatePlot(selectedYear);
-  });
-
-  // Create a separate SVG container for the legend
-  const legendContainer = d3.select("#legend-container")
-    .append("svg")
-    .attr("width", 300)
-    .attr("height", 200);
-
-  // Extract unique years from the data
-  const uniqueYears = Array.from(new Set(data.map(d => d.Year)));
-
-  // Create a legend group with two columns
-  const legendGroup = legendContainer.append("g")
-    .attr("transform", "translate(10, 10)");
-
-  // Set the position for the legend entries
-  const legendRowHeight = 20;
-  const legendColumnWidth = 150;
-  const numRows = Math.ceil(uniqueYears.length / 2);
-
-  // Create legend entries (circles and text)
-  const legendEntries = legendGroup.selectAll(".legend-entry")
-    .data(uniqueYears)
-    .enter()
-    .append("g")
-    .attr("class", "legend-entry")
-    .attr("transform", (d, i) => {
-      if (i < numRows) {
-        return `translate(0,${i * legendRowHeight})`;
-      } else {
-        return `translate(${legendColumnWidth},${(i - numRows) * legendRowHeight})`;
-      }
-    });
-
-  // Append colored circles to the legend
-  legendEntries.append("circle")
-    .attr("cx", 6)
-    .attr("cy", 6)
-    .attr("r", 6)
-    .attr("fill", d => colorScale(d));
-
-  // Append text labels to the legend
-  legendEntries.append("text")
-    .attr("x", 20)
+  svg.append("g")
+    .call(d3.axisLeft(y))
+    .append("text")
+    .attr("class", "axis-title")
+    .attr("x", -10)
     .attr("y", 10)
-    .text(d => d)
-    .style("font-family", "Arial")
-    .style("font-size", "10px");
+    .attr("transform", "rotate(-90)")
+    .style("text-anchor", "end")
+    .text("Height (cm)");
+
+  // Tooltip
+  const tooltip = d3.select("body").append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
+
+  // Scatter plot
+  svg.selectAll("dot")
+    .data(data.filter(d => d.Age && d.Height)) // Filter out missing data
+    .enter()
+    .append("circle")
+    .attr("cx", d => x(d.Age))
+    .attr("cy", d => y(d.Height))
+    .attr("r", 3)
+    .style("fill", "#69b3a2")
+    .on("mouseover", function(event, d) {
+      tooltip.transition()
+        .duration(200)
+        .style("opacity", .9);
+      tooltip.html("Name: " + d.Name + "<br/>" +
+                   "Age: " + d.Age + "<br/>" +
+                   "Height: " + d.Height + " cm<br/>" +
+                   "Team: " + d.Team + "<br/>" +
+                   "Sport: " + d.Sport)
+        .style("left", (event.pageX + 5) + "px")
+        .style("top", (event.pageY - 28) + "px");
+    })
+    .on("mouseout", function() {
+      tooltip.transition()
+        .duration(500)
+        .style("opacity", 0);
+    });
 });
