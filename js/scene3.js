1734267a9b8815730c023da69ce10747aa8f32b9
AishiHuangUIUC <90989610+AishiHuangUIUC@users.noreply.github.com>
2024-07-17 18:18:24 -0500

    Update scene3.js

--- js/scene3.js
+++ js/scene3.js
@@ -1,83 +1,109 @@
 // Load the CSV data
 d3.csv("data/athlete_events.csv").then(function(data) {
-  // Group data by athlete and count medals
-  const athleteMedals = d3.rollup(data, v => v.length, d => d.Name, d => d.Medal);
-  const topAthletes = Array.from(athleteMedals, ([name, medals]) => ({
-    name,
-    gold: medals.get('Gold') || 0,
-    silver: medals.get('Silver') || 0,
-    bronze: medals.get('Bronze') || 0,
-    total: (medals.get('Gold') || 0) + (medals.get('Silver') || 0) + (medals.get('Bronze') || 0)
-  }));
-
-  // Sort athletes by total medals
-  topAthletes.sort((a, b) => b.total - a.total);
-
-  const top10Athletes = topAthletes.slice(0, 10); // Take top 10 athletes
+  // Group data by Year
+  const yearData = d3.nest()
+    .key(d => d.Year)
+    .rollup(v => ({
+      totalAthletes: v.length,
+      totalCountries: d3.set(v.map(d => d.NOC)).size(),
+      totalSports: d3.set(v.map(d => d.Sport)).size()
+    }))
+    .entries(data);
 
   const margin = { top: 50, right: 30, bottom: 100, left: 60 };
   const width = 800 - margin.left - margin.right;
   const height = 600 - margin.top - margin.bottom;
 
-  const svg = d3.select("#plot-container")
+  const svg = d3.select("#line-container")
     .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  const x = d3.scaleBand()
-    .domain(top10Athletes.map(d => d.name))
-    .range([0, width])
-    .padding(0.1);
+  // X axis
+  const x = d3.scaleLinear()
+    .domain(d3.extent(yearData, d => d.key))
+    .range([0, width]);
 
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x).tickFormat(d3.format("d")));
+
+  // Y axis
   const y = d3.scaleLinear()
-    .domain([0, d3.max(top10Athletes, d => d.total)])
+    .domain([0, d3.max(yearData, d => d.value.totalAthletes)])
     .nice()
     .range([height, 0]);
 
   svg.append("g")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x))
-    .selectAll("text")
-    .attr("transform", "rotate(-45)")
-    .style("text-anchor", "end");
-
-  svg.append("g")
     .call(d3.axisLeft(y));
 
-  const colors = { gold: "#ffd700", silver: "#c0c0c0", bronze: "#cd7f32" };
+  // Line generator
+  const line = d3.line()
+    .x(d => x(d.key))
+    .y(d => y(d.value.totalAthletes))
+    .curve(d3.curveMonotoneX);
 
-  svg.selectAll(".bar.gold")
-    .data(top10Athletes)
-    .enter()
-    .append("rect")
-    .attr("class", "bar gold")
-    .attr("x", d => x(d.name))
-    .attr("y", d => y(d.gold))
-    .attr("width", x.bandwidth())
-    .attr("height", d => height - y(d.gold))
-    .attr("fill", colors.gold);
+  // Add the line
+  svg.append("path")
+    .datum(yearData)
+    .attr("fill", "none")
+    .attr("stroke", "black")
+    .attr("stroke-width", 2)
+    .attr("d", line);
 
-  svg.selectAll(".bar.silver")
-    .data(top10Athletes)
-    .enter()
-    .append("rect")
-    .attr("class", "bar silver")
-    .attr("x", d => x(d.name))
-    .attr("y", d => y(d.gold + d.silver))
-    .attr("width", x.bandwidth())
-    .attr("height", d => height - y(d.silver))
-    .attr("fill", colors.silver);
+  // Tooltip
+  const tooltip = d3.select("body").append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
 
-  svg.selectAll(".bar.bronze")
-    .data(top10Athletes)
-    .enter()
-    .append("rect")
-    .attr("class", "bar bronze")
-    .attr("x", d => x(d.name))
-    .attr("y", d => y(d.gold + d.silver + d.bronze))
-    .attr("width", x.bandwidth())
-    .attr("height", d => height - y(d.bronze))
-    .attr("fill", colors.bronze);
+  // Add points
+  svg.selectAll(".dot")
+    .data(yearData)
+    .enter().append("circle")
+    .attr("class", "dot")
+    .attr("cx", d => x(d.key))
+    .attr("cy", d => y(d.value.totalAthletes))
+    .attr("r", 5)
+    .on("mouseover", function(event, d) {
+      tooltip.transition()
+        .duration(200)
+        .style("opacity", .9);
+      tooltip.html("Year: " + d.key + "<br/>" +
+                   "Total Athletes: " + d.value.totalAthletes + "<br/>" +
+                   "Total Countries: " + d.value.totalCountries + "<br/>" +
+                   "Total Sports: " + d.value.totalSports)
+        .style("left", (event.pageX + 5) + "px")
+        .style("top", (event.pageY - 28) + "px");
+    })
+    .on("mouseout", function() {
+      tooltip.transition()
+        .duration(500)
+        .style("opacity", 0);
+    });
+
+  // Add axis labels
+  svg.append("text")
+    .attr("class", "axis-title")
+    .attr("x", width / 2)
+    .attr("y", height + 50)
+    .attr("fill", "#000")
+    .attr("font-weight", "bold")
+    .attr("text-anchor", "middle")
+    .text("Year")
+    .style("font-family", "Arial")
+    .style("font-size", "12px");
+
+  svg.append("text")
+    .attr("class", "axis-title")
+    .attr("x", -height / 2)
+    .attr("y", -50)
+    .attr("fill", "#000")
+    .attr("font-weight", "bold")
+    .attr("text-anchor", "middle")
+    .attr("transform", "rotate(-90)")
+    .text("Total Athletes")
+    .style("font-family", "Arial")
+    .style("font-size", "12px");
 });
