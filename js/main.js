183b83f0d667c3f78d719b5ebbc2c499a633aa5b
AishiHuangUIUC <90989610+AishiHuangUIUC@users.noreply.github.com>
2024-07-17 19:19:58 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,27 +1,27 @@
-// main.js
+// // main.js
 
-let currentScene = 1;
-let olympicData = null;
+// let currentScene = 1;
+// let olympicData = null;
 
-function updateScene() {
-  if (currentScene === 1) {
-    renderScene1(olympicData);
-  } else if (currentScene === 2) {
-    renderScene2(olympicData);
-  } else if (currentScene === 3) {
-    renderScene3(olympicData);
-  }
-}
+// function updateScene() {
+//   if (currentScene === 1) {
+//     renderScene1(olympicData);
+//   } else if (currentScene === 2) {
+//     renderScene2(olympicData);
+//   } else if (currentScene === 3) {
+//     renderScene3(olympicData);
+//   }
+// }
 
-// Load data and initialize visualization
-d3.csv("data/athlete_events.csv").then(data => {
-  olympicData = data;
-  updateScene();
-});
+// // Load data and initialize visualization
+// d3.csv("data/athlete_events.csv").then(data => {
+//   olympicData = data;
+//   updateScene();
+// });
 
-document.addEventListener("keydown", (event) => {
-  if (event.key === "ArrowRight") {
-    currentScene = (currentScene % 3) + 1;
-    updateScene();
-  }
-});
+// document.addEventListener("keydown", (event) => {
+//   if (event.key === "ArrowRight") {
+//     currentScene = (currentScene % 3) + 1;
+//     updateScene();
+//   }
+// });
