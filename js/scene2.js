cb902b3f96322a532a42957504ff8f8dae3c0879
AishiHuangUIUC <90989610+AishiHuangUIUC@users.noreply.github.com>
2024-07-20 22:08:05 -0500

    Update scene2.js

--- js/scene2.js
+++ js/scene2.js
@@ -1,109 +1,3 @@
-// d3.csv("data/athlete_events.csv").then(function(data) {
-//     // Filter out rows without medals
-//     const medalData = data.filter(d => d.Medal);
-
-//     // Aggregate medals by country and type
-//     const medalsByCountryAndType = d3.rollup(medalData,
-//         v => ({
-//             gold: v.filter(d => d.Medal === 'Gold').length,
-//             silver: v.filter(d => d.Medal === 'Silver').length,
-//             bronze: v.filter(d => d.Medal === 'Bronze').length,
-//             total: v.length
-//         }),
-//         d => d.NOC
-//     );
-
-//     // Convert to array and sort
-//     const sortedMedalsByCountry = Array.from(medalsByCountryAndType, ([country, medals]) => ({ country, ...medals }))
-//         .sort((a, b) => d3.descending(a.total, b.total))
-//         .slice(0, 15);
-
-//     // Basic dimensions
-//     const margin = { top: 20, right: 30, bottom: 100, left: 60 };
-//     const width = 800 - margin.left - margin.right;
-//     const height = 600 - margin.top - margin.bottom;
-
-//     // Append SVG to the plot-container
-//     const svg = d3.select("#plot-container")
-//         .append("svg")
-//         .attr("width", width + margin.left + margin.right)
-//         .attr("height", height + margin.top + margin.bottom)
-//         .append("g")
-//         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-//     // Scales
-//     const xScale = d3.scaleBand()
-//         .domain(sortedMedalsByCountry.map(d => d.country))
-//         .range([0, width])
-//         .padding(0.1);
-
-//     const yScale = d3.scaleLinear()
-//         .domain([0, d3.max(sortedMedalsByCountry, d => d.total)])
-//         .nice()
-//         .range([height, 0]);
-
-//     const colorScale = d3.scaleOrdinal()
-//         .domain(['gold', 'silver', 'bronze'])
-//         .range(['#ffd700', '#c0c0c0', '#cd7f32']);
-
-//     // Axes
-//     svg.append("g")
-//         .attr("transform", "translate(0," + height + ")")
-//         .call(d3.axisBottom(xScale))
-//         .selectAll("text")
-//         .attr("transform", "rotate(-45)")
-//         .style("text-anchor", "end");
-
-//     svg.append("g")
-//         .call(d3.axisLeft(yScale));
-
-//     // Stack layout
-//     const stack = d3.stack()
-//         .keys(['gold', 'silver', 'bronze'])
-//         .order(d3.stackOrderNone)
-//         .offset(d3.stackOffsetNone);
-
-//     const stackedData = stack(sortedMedalsByCountry.map(d => ({ country: d.country, gold: d.gold, silver: d.silver, bronze: d.bronze })));
-
-//     // Bars
-//     const bars = svg.selectAll(".layer")
-//         .data(stackedData)
-//         .enter()
-//         .append("g")
-//         .attr("class", "layer")
-//         .style("fill", d => colorScale(d.key));
-
-//     bars.selectAll("rect")
-//         .data(d => d)
-//         .enter()
-//         .append("rect")
-//         .attr("x", d => xScale(d.data.country))
-//         .attr("y", d => yScale(d[1]))
-//         .attr("height", d => yScale(d[0]) - yScale(d[1]))
-//         .attr("width", xScale.bandwidth())
-//         .on("mouseover", function(event, d) {
-//             const [mouseX, mouseY] = d3.pointer(event);
-//             const key = d3.select(this.parentNode).datum().key;
-//             tooltip.style("display", "block")
-//                 .html(`Country: ${d.data.country}<br>${key.charAt(0).toUpperCase() + key.slice(1)} Medals: ${d.data[key]}`)
-//                 .style("left", (mouseX + 10) + "px")
-//                 .style("top", (mouseY + 10) + "px");
-//         })
-//         .on("mouseout", function() {
-//             tooltip.style("display", "none");
-//         });
-
-//     // Tooltip
-//     const tooltip = d3.select("body").append("div")
-//         .attr("class", "tooltip")
-//         .style("display", "none");
-
-// }).catch(function(error) {
-//     console.error("Error loading the CSV file:", error);
-// });
-
-
-
 d3.csv("data/athlete_events.csv").then(function(data) {
     const medalData = data.filter(d => d.Medal);
 
@@ -121,7 +15,8 @@ d3.csv("data/athlete_events.csv").then(function(data) {
         .sort((a, b) => d3.descending(a.total, b.total))
         .slice(0, 15);
 
-    const margin = { top: 20, right: 30, bottom: 100, left: 60 };
+    // const margin = { top: 20, right: 30, bottom: 100, left: 60 };
+    const margin = { top: 20, right: 20, bottom: 60, left: 80 };
     const width = 800 - margin.left - margin.right;
     const height = 600 - margin.top - margin.bottom;
 
